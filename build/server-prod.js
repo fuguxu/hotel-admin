0aa391066d84894787e9a78443183fec2873ed38
fuguxu <18772315667@163.com>
2020-04-13 09:21:23 +0800

    fix eslint

--- build/server-prod.js
+++ build/server-prod.js
@@ -8,23 +8,22 @@ const webpackConfig = require('./webpack.pro.config')
 const spinner = ora('start building....')
 spinner.start()
 
-webpack(webpackConfig, (err, stats)=>{
+webpack(webpackConfig, (err, stats) => {
   spinner.stop()
-  if(err) throw err
+  if (err) throw err
 
   process.stdout.write(stats.toString({
-    colors:true,
-    modules:false,
-    children:false,
-    chunkModules:false,
-    chunks:false
+    colors: true,
+    modules: false,
+    children: false,
+    chunkModules: false,
+    chunks: false
   }))
 
-  if(stats.hasErrors()){
+  if (stats.hasErrors()) {
     console.log(chalk.red('build failed with errors!'))
     process.exit(1)
   }
 
   console.log(chalk.cyan('build finished!'))
-
 })
